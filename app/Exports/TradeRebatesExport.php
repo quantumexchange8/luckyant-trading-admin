d76c41ee0d8bd918719169bfee8e9bf8b917ba4b
unknown <demondrake00@gmail.com>
2024-04-04 11:06:46 +0800

    Update TradeRebatesExport.php
    make it can export at least 30000 data

--- app/Exports/TradeRebatesExport.php
+++ app/Exports/TradeRebatesExport.php
@@ -4,16 +4,20 @@ namespace App\Exports;
 
 use Carbon\Carbon;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithChunkReading;
 
-class TradeRebatesExport implements FromCollection, WithHeadings, ShouldQueue
+class TradeRebatesExport implements FromCollection, WithHeadings, ShouldQueue, WithChunkReading
 {
     private $query;
+    private $chunkSize;
 
-    public function __construct($query)
+    public function __construct($query, $chunkSize = 2000)
     {
         $this->query = $query;
+        $this->chunkSize = $chunkSize;
+        ini_set('memory_limit', '1G');
     }
 
     /**
@@ -21,26 +25,27 @@ class TradeRebatesExport implements FromCollection, WithHeadings, ShouldQueue
      */
     public function collection(): \Illuminate\Support\Collection
     {
-        $records = $this->query->get();
-        $result = array();
-        foreach($records as $row){
-
-            $result[] = array(
-                'date' => Carbon::parse($row->created_at)->format('Y-m-d H:i:s'),
-                'name' => $row->upline_user->name,
-                'email' => $row->upline_user->email,
-                'affiliate_name' => $row->user->email,
-                'affiliate_email' => $row->user->email,
-                'trade_volume' => $row->trade_volume,
-                'net_rebate_amt' => $row->net_rebate_amt,
-                'rebate_final_amt_get' => $row->rebate_final_amt_get,
-                'status' => $row->is_claimed,
-            );
-        }
-
-        return collect($result);
+        $result = collect();
+    
+        $this->query->chunk($this->chunkSize, function ($records) use ($result) {
+            foreach ($records as $row) {
+                $result->push([
+                    'date' => Carbon::parse($row->created_at)->format('Y-m-d H:i:s'),
+                    'name' => $row->upline_user->name,
+                    'email' => $row->upline_user->email,
+                    'affiliate_name' => $row->user->name,
+                    'affiliate_email' => $row->user->email,
+                    'trade_volume' => $row->trade_volume,
+                    'net_rebate_amt' => $row->net_rebate_amt,
+                    'rebate_final_amt_get' => $row->rebate_final_amt_get,
+                    'status' => $row->is_claimed,
+                ]);
+            }
+        });
+    
+        return $result;
     }
-
+    
     public function headings(): array
     {
         return [
@@ -54,5 +59,10 @@ class TradeRebatesExport implements FromCollection, WithHeadings, ShouldQueue
             'Total Rebate Earn',
             'Status',
         ];
+    }
+
+    public function chunkSize(): int
+    {
+        return $this->chunkSize;
     }
 }
