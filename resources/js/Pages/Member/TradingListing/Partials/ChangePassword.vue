2a0f3a1a64c9030ab124ead3a14a0d8491a3ba91
unknown <demondrake00@gmail.com>
2024-04-15 17:38:42 +0800

    Fixed change password error
    show password rules

--- resources/js/Pages/Member/TradingListing/Partials/ChangePassword.vue
+++ resources/js/Pages/Member/TradingListing/Partials/ChangePassword.vue
@@ -48,28 +48,56 @@ const closeModal = () => {
 }
 
 const passwordRules = [
-    { message: 'change_password_terms_1', regex: /[A-Z]+/ },
-    { message: 'change_password_terms_3', regex: /[0-9]+/ },
+    { message: 'register_terms_1', regex: /.{6,}/ },
+    { message: 'register_terms_2', regex: /[A-Z]+/ },
+    { message: 'register_terms_3', regex: /[a-z]+/ },
+    { message: 'register_terms_4', regex: /[0-9]+/ },
+    { message: 'register_terms_5', regex: /[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]+/ }
 ];
 
 const passwordValidation = () => {
-    let valid = false;
-    let messages = [];
+    let masterValid = false;
+    let investorValid = false;
+    let masterMessages = [];
+    let investorMessages = [];
 
-    for (let condition of passwordRules) {
-        const isConditionValid = condition.regex.test(form.master_password);
-        
-        if (isConditionValid) {
-            valid = true;
+    // Check if either master_password or investor_password has data
+    const hasMasterPassword = form.master_password !== '';
+    const hasInvestorPassword = form.investor_password !== '';
+
+    // Validate master password
+    if (hasMasterPassword) {
+        for (let condition of passwordRules) {
+            const isConditionValid = condition.regex.test(form.master_password);
+
+            if (isConditionValid) {
+                masterValid = true;
+            }
+
+            masterMessages.push({
+                message: condition.message,
+                valid: isConditionValid,
+            });
         }
-
-        messages.push({
-            message: condition.message,
-            valid: isConditionValid,
-        });
     }
 
-    return { valid, messages };
+    // Validate investor password
+    if (hasInvestorPassword) {
+        for (let condition of passwordRules) {
+            const isConditionValid = condition.regex.test(form.investor_password);
+
+            if (isConditionValid) {
+                investorValid = true;
+            }
+
+            investorMessages.push({
+                message: condition.message,
+                valid: isConditionValid,
+            });
+        }
+    }
+
+    return { masterValid, masterMessages, investorValid, investorMessages };
 };
 
 </script>
@@ -106,6 +134,29 @@ const passwordValidation = () => {
                 <InputError :message="form.errors.master_password" class="mt-2" />
             </div>
         </div>
+        <div v-if="form.master_password" class="flex flex-col items-start gap-3 self-stretch py-2">
+            <div v-for="message in passwordValidation().masterMessages" :key="message.key" class="flex items-center gap-2 self-stretch">
+                <div
+                    :class="{
+                            'bg-success-500': message.valid,
+                            'bg-gray-400 dark:bg-dark-eval-3': !message.valid
+                        }"
+                    class="flex justify-center items-center w-5 h-5 rounded-full grow-0 shrink-0"
+                >
+                    <CheckIcon aria-hidden="true" class="text-white" />
+                </div>
+                <div
+                    class="text-sm"
+                    :class="{
+                            'text-gray-600 dark:text-gray-300': message.valid,
+                            'text-gray-400 dark:text-gray-500': !message.valid
+                        }"
+                >
+                    {{ $t('public.' + message.message) }}
+                </div>
+            </div>
+        </div>
+
         <div class="grid grid-cols-3 items-center gap-2">
             <Label 
                 for="investor_password"
@@ -136,8 +187,8 @@ const passwordValidation = () => {
                 <InputError :message="form.errors.investor_password" class="mt-2" />
             </div>
         </div>
-        <div  class="flex flex-col items-start gap-3 self-stretch">
-            <div v-for="message in passwordValidation().messages" :key="message.key" class="flex items-center gap-2 self-stretch">
+        <div v-if="form.investor_password" class="flex flex-col items-start gap-3 self-stretch py-2">
+            <div v-for="message in passwordValidation().investorMessages" :key="message.key" class="flex items-center gap-2 self-stretch">
                 <div
                     :class="{
                             'bg-success-500': message.valid,
@@ -158,6 +209,7 @@ const passwordValidation = () => {
                 </div>
             </div>
         </div>
+
     </div>
     <div class="grid grid-cols-1 items-center gap-2">
         <div class="mt-6 flex justify-end">
