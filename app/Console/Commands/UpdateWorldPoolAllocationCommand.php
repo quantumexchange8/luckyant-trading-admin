985f43c4dc319c8f6cf31968f4513c71987fc8f8
waynelai <waynelai06@gmail.com>
2025-05-16 14:23:43 +0800

    fix: world pool calculation

--- app/Console/Commands/UpdateWorldPoolAllocationCommand.php
+++ app/Console/Commands/UpdateWorldPoolAllocationCommand.php
@@ -18,16 +18,19 @@ class UpdateWorldPoolAllocationCommand extends Command
     {
         $today_pool = WorldPoolAllocation::whereDate('allocation_date', today())->first();
 
-        $last_pool_amount = WorldPoolAllocation::whereDate('allocation_date', '<', today())
-            ->latest()
-            ->first();
-
         if (!$today_pool) {
             $today_pool = WorldPoolAllocation::create([
                 'allocation_date' => today(),
-                'allocation_amount' => $last_pool_amount->allocation_amount,
+                'allocation_amount' => 0,
             ]);
         }
+
+        $last_pool_amount = WorldPoolAllocation::whereDate('allocation_date', '<', today())
+            ->latest()
+            ->first()
+            ->allocation_amount;
+
+        $second_last_pool_amount = (float) $today_pool->allocation_amount ?? 0;
 
         $active_subscriptions_capital = Subscription::where('status', 'Active')
             ->sum('meta_balance');
@@ -39,8 +42,7 @@ class UpdateWorldPoolAllocationCommand extends Command
             })
             ->sum('subscription_amount');
 
-        $total_extra_amount = WorldPoolAllocation::whereDate('allocation_date', '<=', Carbon::now())
-            ->sum('allocation_amount');
+        $total_extra_amount = (float) $last_pool_amount - ($second_last_pool_amount);
 
         $world_pool = ($active_pamm_capital + $active_subscriptions_capital + $total_extra_amount) / 10000 * 0.4;
 
