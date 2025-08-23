a8d8cc4c7624b2039585a25a7e16d00f8b21df7b
quantumexchange8 <96815929+quantumexchange8@users.noreply.github.com>
2025-08-24 01:13:03 +0800

    Google Tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -23,8 +23,20 @@
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
+
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WKP9LMS5');</script>
+        <!-- End Google Tag Manager -->
     </head>
     <body class="font-sans antialiased bg-gray-100 dark:bg-gray-950 ">
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WKP9LMS5"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
         @inertia
     </body>
 </html>
