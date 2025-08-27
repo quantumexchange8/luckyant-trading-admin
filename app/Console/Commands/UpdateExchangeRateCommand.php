b2f7ceb8be63730548dd7ca8f78805e1cba0c6e5
waynelai <waynelai06@gmail.com>
2025-08-27 13:52:33 +0800

    fix: deposit rate

--- app/Console/Commands/UpdateExchangeRateCommand.php
+++ app/Console/Commands/UpdateExchangeRateCommand.php
@@ -22,7 +22,7 @@ class UpdateExchangeRateCommand extends Command
 
         foreach ($results as $currency => $rate) {
             $baseCurrency = CurrencyConversionRate::where('base_currency', $currency)->first();
-            $depositRate = $rate * 1.015; // Deposit +1.5%
+            $depositRate = $rate * 1.018; // Deposit +1.5%
 //            $withdrawalRate = $rate * 0.99;
 
             $baseCurrency->update([
